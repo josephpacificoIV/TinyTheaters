b1aa9651159007a6fa2dbf4f14fb8b0f7796fdcc
josephpacificoIV <joejpacifico@gmail.com>
2022-07-23 12:42:36 -0400

    finished update, now on delete

--- rcttc-data.sql
+++ rcttc-data.sql
@@ -3,25 +3,30 @@ use TinyTheaters;
 -- With our imported data and target schema in the same database, we can write SQL to select from the imported table 
 -- and insert into the target tables. Always start with a select statement, confirm you have the results you want, 
 -- then complete the statement with an insert into. Never write an untested insert into... select and hope for the best.
+-- if you end up with more than 74 rows, delete all rows and re-run the insert
 
 -- confirm before the insert for NN columns, let database generate customer_id 
+
+
+
+-- Customer
+
 select distinct customer_first, customer_last, customer_email, customer_phone, customer_address
 from `rcttc-data`;
 
--- if you end up with more than 74 rows, delete all rows and re-run the insert
 insert into Customer(customer_first, customer_last, customer_email, customer_phone, customer_address)
 select distinct customer_first, customer_last, customer_email, customer_phone, customer_address
 from `rcttc-data`;
 
 select * from Customer;
 
--- ======================Show
+-- Theater
+
 select * from Theater;
 -- check first
 select distinct theater, theater_address, theater_phone, theater_email
 from `rcttc-data`;
 
--- Theater
 insert into Theater(theater_name, theater_address, theater_phone, theater_email)
 select distinct theater, theater_address, theater_phone, theater_email
 from `rcttc-data`;
@@ -47,8 +52,7 @@ select * from `Show`;
 
 -- TicketPurchase
 
-select * from Theater;
-select * from `rcttc-data`;
+select * from TicketPurchase;
 
 select distinct d.seat, c.customer_id, s.show_id
 from `rcttc-data` d
@@ -66,10 +70,87 @@ inner join `Show` s on s.show_name = d.show and s.date = d.date and s.theater_id
 
 select * from TicketPurchase;
 
+-- drop 
+drop table `rcttc-data`;
 
+-- ==================UPDATE=========================
 
+-- update #1
+select * from `Show`;
 
+update `Show` set
+    ticket_price = 22.25
+where show_id = 5;
 
+select * from `Show`;
+
+-- update #2
+
+-- confirm
+select * 
+from TicketPurchase t
+left outer join Customer c on c.customer_id = t.customer_id
+where show_id = 5
+order by seat_label;
+
+-- update
+update TicketPurchase set
+    seat_label = 'B4'
+where ticket_purchase_id = 98;
+
+update TicketPurchase set
+    seat_label = 'C2'
+where ticket_purchase_id = 100;
+
+update TicketPurchase set
+    seat_label = 'A4'
+where ticket_purchase_id = 101;
+
+select * 
+from TicketPurchase t
+left outer join Customer c on c.customer_id = t.customer_id
+where show_id = 5
+order by seat_label;
+
+-- update #3 
+select * 
+from Customer 
+where customer_first = 'Jammie' and customer_last = 'Swindles';
+
+-- turn off safe updates
+set sql_safe_updates = 0;
+
+-- update
+update Customer set 
+customer_phone = '1-801-EAT-CAKE' 
+where customer_first = 'Jammie' and customer_last = 'Swindles';
+
+-- turn on safe updates
+set sql_safe_updates = 1;
+
+select * 
+from Customer 
+where customer_first = 'Jammie' and customer_last = 'Swindles';
+
+-- ==================DELETE=========================
+
+-- 87 rows of tickets purchase at 10 pins
+select r.theater_name, t.customer_id, count(customer_id) -- s.show_name, s.date, t.customer_id, r.theater_name
+from TicketPurchase t
+left outer join `Show` s on s.show_id = t.show_id
+left outer join Theater r on r.theater_id = s.theater_id
+group by t.customer_id, r.theater_name
+-- group by t.customer_id, s.show_name, s.date, r.theater_name
+-- having r.theater_name = '10 Pin';
+having r.theater_name = '10 Pin' and count(customer_id) = 1;
+
+--  should have 3 single-ticket reservations for 10 Pin on 2021-01-04 show The Dress
+select *
+from TicketPurchase t
+left outer join `Show` s on s.show_id = t.show_id
+left outer join Theater r on r.theater_id = s.theater_id
+where r.theater_name = '10 Pin' and show_name = 'The Dress'
+order by customer_id, seat_label;
 
 
 -- -- disable safe updates
