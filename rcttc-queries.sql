eb585106b7acdb844f293450fe55caf97e8aaf2a
josephpacificoIV <joejpacifico@gmail.com>
2022-07-24 14:06:31 -0400

    sql queries complete

--- rcttc-queries.sql
+++ rcttc-queries.sql
@@ -1,3 +1,5 @@
+use TinyTheaters;
+
 -- Find all performances in the last quarter of 2021 (Oct. 1, 2021 - Dec. 31 2021).
 select * 
 from `Show` s
@@ -58,3 +60,41 @@ left outer join Theater t on t.theater_id = s.theater_id;
 
 
 -- Count total tickets purchased per customer.
+select tp.customer_id, c.customer_first, c.customer_last, count(tp.ticket_purchase_id) total_tickets
+from TicketPurchase tp
+left outer join Customer c on c.customer_id = tp.customer_id
+group by customer_id, c.customer_first, c.customer_last;
+
+-- Calculate the total revenue per show based on tickets sold.
+
+select s.show_name,
+count(tp.customer_id) as total_customers,
+cast(sum(s.ticket_price * 1) as decimal(5,2)) total_revenue
+from TicketPurchase tp
+left outer join `Show` s on s.show_id = tp.show_id
+group by s.show_name
+order by total_revenue;
+
+
+
+-- Calculate the total revenue per theater based on tickets sold.
+select t.theater_name,
+count(tp.customer_id) as total_customers,
+cast(sum(s.ticket_price * 1) as decimal(10,2)) total_revenue
+from TicketPurchase tp
+left outer join `Show` s on s.show_id = tp.show_id
+left outer join Theater t on t.theater_id = s.theater_id
+group by t.theater_name
+order by total_revenue;
+
+-- Who is the biggest supporter of RCTTC? Who spent the most in 2021?
+
+select concat(c.customer_first, ' ', c.customer_last) customer,
+count(tp.customer_id) as total_tickets_bought,
+cast(sum(s.ticket_price * 1) as decimal(5,2)) total_cost
+from TicketPurchase tp
+left outer join `Show` s on s.show_id = tp.show_id
+left outer join Customer c on c.customer_id = tp.customer_id
+group by concat(c.customer_first, ' ', c.customer_last)
+order by total_cost desc
+limit 1;
